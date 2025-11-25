e22c1e2d355b1156411560fcdee71bfda72a2a88
Sorin08 <stansorin8@yahoo.ro>
2025-11-25 21:57:14 +0200

    Create farmAclassic.js

---
+++ farmAclassic.js
@@ -0,0 +1,1 @@
+javascript:var progressElm=$("<div>da</div>");$("#am_widget_Farm").before(progressElm);var total=$(".farm_icon_a").length;var farms=$(".farm_icon_a");var fasend=function(a){if(a==total)return;$(farms[a]).trigger("click");progressElm.html(a+" / "+total);setTimeout("fasend("+a+"+1)",500+Math.random(500))};fasend(0);
